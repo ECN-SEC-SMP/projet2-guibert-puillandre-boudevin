1678944b06f3ec48ffecf6e69693a9dc35a504b5
ValentinBoudevin <valentin.boudevin@gmail.com>
2022-03-18 10:22:56 +0100

    Cartees

--- Cartes.hpp
+++ Cartes.hpp
@@ -23,17 +23,17 @@ class Cartes {
       void set_couleur(Couleur_carte couleur);
       Couleur_carte get_couleur() const;
 
-    friend ostream& operator<<(ostream& s,Cartes const& v);
+      friend ostream& operator<<(ostream& s,Cartes const& v);
 
 
     protected:
 
         Couleur_carte couleur_carte;
-        int nb_cases
+        int nb_cases;
 
     private:
 
-      conv_couleur_j_cartes()
+        void conv_couleur_j_cartes();
 
 };
 
